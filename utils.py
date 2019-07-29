74d2bd13f720ff7d7a5cbcc2bc4f88b8081aea4f
Salinder Sidhu <salinder.sid@gmail.com>
2019-07-29 01:24:23 -0400

    code cleanup

--- utils.py
+++ utils.py
@@ -3,16 +3,16 @@ from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
 def merge_sentences(sentence_a, sentence_b):
-	frag_a = sentence_a.split(' ')
-	frag_b = sentence_b.split(' ')
-	overlap = [x for x in frag_a if x in frag_b]
-	frag_overlap_start = frag_a.index(overlap[0])
-	frag_overlap_end = len(frag_b) - frag_b[::-1].index(overlap[-1])
-	return ' '.join(frag_a[0:frag_overlap_start] + overlap + frag_b[frag_overlap_end:])
+	words_a = sentence_a.split(' ')
+	words_b = sentence_b.split(' ')
+	overlap = [x for x in words_a if x in words_b]
+	start_point = words_a.index(overlap[0])
+	end_point = len(words_b) - words_b[::-1].index(overlap[-1])
+	return ' '.join(words_a[0:start_point] + overlap + words_b[end_point:])
 
-def convert_audio(file_in, file_out, **kwargs):
-	src = os.path.join(os.getcwd(), file_in)
-	dst = os.path.join(os.getcwd(), file_out)
+def convert_audio_file(fname_in, fname_out, **kwargs):
+	src = os.path.join(os.getcwd(), fname_in)
+	dest = os.path.join(os.getcwd(), fname_out)
 	src_ext = os.path.splitext(src)[1][1:]
 	audio = AudioSegment.from_file(src, src_ext)
-	audio.export(dst, **kwargs)
+	audio.export(dest, **kwargs)
